8050790f8fcc322d854541bd5554dcbdde74bfde
MaxiPineker <43345908+MaxiPineker@users.noreply.github.com>
2020-06-15 09:33:31 +0200

    Update GermanStyleDateChecker.java

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
+    private static String regex="^((0?\\d)|((1|2)\\d)|(3(0|1)))" +
+                                "(\\s|\\-|\\.)" +
+                                "((0?\\d)|(1\\d[0-2]))" +
+                                "(\\s|\\-|\\.)" +
+                                "((19|20)?\\d\\d)$";
+    private static Pattern pattern=Pattern.compile(regex);
+
     public static Matcher getMatcher(String dateString) {
-        return null;
+        return pattern.matcher(dateString);
     }
 }
